3ecad377dccaec8f0260322d03206dbd7a0b0853
Samuel <signfforce@gmail.com>
2024-09-24 17:57:40 +0300

    frontend commit

---
+++ front/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      samuel mumo
+      <h1>you are the app component</h1>
+    </>
+  )
+}
+
+export default App
